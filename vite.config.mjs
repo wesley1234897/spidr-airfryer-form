3360f1defce3c336370f469499e5861a6f2bcbd8
wesley1234897 <93157211+wesley1234897@users.noreply.github.com>
2025-07-15 18:34:31 -0500

    updates

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Replace with your actual repo name:
 export default defineConfig({
+  base: '/spidr-airfryer-form/',
   plugins: [react()],
 });
